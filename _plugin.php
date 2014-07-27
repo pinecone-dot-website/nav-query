a2782e07e507bc0a24f6766644dc83da7a9980b7
postpostmodern <postpostmodern@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-27 21:03:21 +0000

    update to 0.5
    git-svn-id: http://plugins.svn.wordpress.org/nav-query/trunk@955668 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ _plugin.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name:	NAV QUERY
+Plugin URI:		http://wordpress.org/plugins/nav-query/
+Description:	create dynamic nav menus using wp_query
+Author:			postpostmodern, pinecone-dot-io
+Text Domain:	nav-query
+Domain Path:	/lang
+Version:		0.5
+Author URI:		http://pinecone.io/
+*/
+
+register_activation_hook( __FILE__, create_function("", '$ver = "5.3"; if( version_compare(phpversion(), $ver, "<") ) die( "This plugin requires PHP version $ver or greater be installed." );') );
+
+require __DIR__.'/index.php';
